82e82341d7658764598b79d039e917b74263df23
Maxuan <786609202@qq.com>
2021-01-22 16:08:59 +0800

    change route type

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import Home  from  "./components/Home"
 import Navgatitor from "./components/Navgatitor"
 import Registe from "./components/Reginste"
 import Login from "./components/Login"
-import { BrowserRouter, Route,Switch } from 'react-router-dom'
+import { HashRouter, Route,Switch } from 'react-router-dom'
 import "antd/dist/antd.css"
 
 function App() {
   return (
-    <BrowserRouter basename='/eth-front/build/'>
+    <HashRouter basename='/eth-front/build/'>
       <div className="App">
         <Switch>
           <Route path="/" exact component={Login} />
@@ -17,7 +17,7 @@ function App() {
           <Route path="/registe" exact component={Registe} />
         </Switch>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
